9011e2c6ddef97d55f4710933888a8f673c1eec2
Warwick Stone <warwick_stone@fastmail.com>
2021-09-26 14:01:28 +1000

    add immutable collection examples

---
+++ Snippets.Test/ImmutableCollections/Equality.cs
@@ -0,0 +1,66 @@
+using System.Collections.Immutable;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Snippets.Test.ImmutableCollections
+{
+    class Equality
+    {
+        [Test]
+        public void Adding_to_list_creates_new_list()
+        {
+            var list = ImmutableList.Create(1, 2, 3);
+            var updatedList = list.Add(4);
+
+            Assert.AreNotSame(list, updatedList);
+        }
+
+        [Test]
+        public void Copied_list_is_not_same()
+        {
+            var list = ImmutableList.Create(1, 2, 3);
+            var copy = ImmutableList.Create(list.ToArray());
+
+            Assert.AreNotSame(list, copy);
+        }
+
+        [Test]
+        public void Copied_list_is_equal()
+        {
+            var list = ImmutableList.Create(1, 2, 3);
+            var copy = ImmutableList.Create(list.ToArray());
+
+            Assert.AreEqual(list, copy);
+        }
+
+        [Test]
+        public void Copied_list_reference_type_items_are_not_cloned()
+        {
+            var myRef = new ExampleReferenceType();
+            var list = ImmutableList.Create(myRef);
+            var copy = ImmutableList.Create(list.ToArray());
+
+            Assert.AreEqual(list, copy);
+            Assert.AreSame(list[0], copy[0]);
+        }
+
+        [Test]
+        public void Copied_list_value_type_items_are_cloned()
+        {
+            var myRef = new ExampleValueType();
+            var list = ImmutableList.Create(myRef);
+            var copy = ImmutableList.Create(list.ToArray());
+
+            Assert.AreEqual(list, copy);
+            Assert.AreNotSame(list[0], copy[0]);
+        }
+    }
+
+    struct ExampleValueType
+    {
+    }
+
+    class ExampleReferenceType
+    {
+    }
+}
