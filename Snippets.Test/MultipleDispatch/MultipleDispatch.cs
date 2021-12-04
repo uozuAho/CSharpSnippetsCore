42268f6cbb45d9816519c19a6a68ebe226e11419
Warwick Stone <warwick_stone@fastmail.com>
2021-12-04 21:39:48 +1100

    WIP: multi dispatch comparer

---
+++ Snippets.Test/MultipleDispatch/MultipleDispatch.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Snippets.Test.MultipleDispatch
+{
+    interface ICommand
+    {
+    }
+
+    internal class StuffCommand : ICommand
+    {
+    }
+
+    internal class AsdfCommand : ICommand
+    {
+    }
+
+    internal class ActualCommandComparer
+    {
+        private const int Less = -1;
+        private const int Same = 0;
+        private const int Greater = 1;
+        private const int Unmatched = 2;
+
+        public static int Compare(ICommand a, ICommand b, int _ = 0) => Compare((dynamic)a, (dynamic)b);
+
+        // private static int Compare(ICommand a, ICommand b) => Same;
+        private static int Compare<T>(T a, T b) where T : ICommand => Same;
+        private static int Compare(AsdfCommand a, StuffCommand b) => Greater;
+    }
+
+    internal class CommandComparer : IComparer<ICommand>
+    {
+        public int Compare(ICommand x, ICommand y)
+        {
+            return ActualCommandComparer.Compare(x, y);
+        }
+    }
+
+    internal class ComparerTests
+    {
+        private CommandComparer _comparer;
+
+        [SetUp]
+        public void Setup()
+        {
+            _comparer = new CommandComparer();
+        }
+
+        [Test]
+        public void Same_commands_are_equal()
+        {
+            Assert.That(new StuffCommand(), Is.EqualTo(new StuffCommand()).Using(_comparer));
+            Assert.That(new AsdfCommand(), Is.EqualTo(new AsdfCommand()).Using(_comparer));
+        }
+
+        [Test]
+        public void Asdf_is_greater_than_stuff()
+        {
+            Assert.That(new AsdfCommand(), Is.GreaterThan(new StuffCommand()).Using(_comparer));
+        }
+
+        // [Test]
+        // public void Stuff_is_less_than_asdf()
+        // {
+        //     Assert.That(new StuffCommand(), Is.GreaterThan(new AsdfCommand()).Using(_comparer));
+        // }
+    }
+}
