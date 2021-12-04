d10e6d3279b6f606060b5999160baacdba0cf079
Warwick Stone <warwick_stone@fastmail.com>
2021-12-04 21:51:22 +1100

    single comparer class

--- Snippets.Test/MultipleDispatch/MultipleDispatch.cs
+++ Snippets.Test/MultipleDispatch/MultipleDispatch.cs
@@ -8,71 +8,69 @@ namespace Snippets.Test.MultipleDispatch
     {
     }
 
-    internal class StuffCommand : ICommand
+    internal class SmallCommand : ICommand
     {
     }
 
-    internal class AsdfCommand : ICommand
+    internal class BigCommand : ICommand
     {
     }
 
-    internal class ActualCommandComparer
+    internal class MultiDispatchCommandComparer : IComparer<ICommand>
     {
         private const int Less = -1;
         private const int Same = 0;
         private const int Greater = 1;
         private const int Unmatched = 2;
 
-        public static int Compare(ICommand a, ICommand b, int _ = 0)
+        public int Compare(ICommand x, ICommand y)
         {
-            var result = Compare((dynamic)a, (dynamic)b);
+            return CompareMulti(x, y, 0);
+        }
+
+        private static int CompareMulti(ICommand a, ICommand b, int _ = 0)
+        {
+            var result = CompareMulti((dynamic)a, (dynamic)b);
             if (result != Unmatched) return result;
-            result = Compare((dynamic)b, (dynamic)a);
+            result = CompareMulti((dynamic)b, (dynamic)a);
             if (result != Unmatched) return result;
 
             throw new ArgumentException("Undefined comparison");
         }
 
-        private static int Compare(ICommand a, ICommand b) => Unmatched;
-        private static int Compare<T>(T a, T b) where T : ICommand => Same;
-        private static int Compare(AsdfCommand a, StuffCommand b) => Greater;
-    }
-
-    internal class CommandComparer : IComparer<ICommand>
-    {
-        public int Compare(ICommand x, ICommand y)
-        {
-            return ActualCommandComparer.Compare(x, y);
-        }
+        private static int CompareMulti(ICommand a, ICommand b) => Unmatched;
+        private static int CompareMulti<T>(T a, T b) where T : ICommand => Same;
+        private static int CompareMulti(BigCommand a, SmallCommand b) => Greater;
+        
     }
 
     internal class ComparerTests
     {
-        private CommandComparer _comparer;
+        private MultiDispatchCommandComparer _comparer;
 
         [SetUp]
         public void Setup()
         {
-            _comparer = new CommandComparer();
+            _comparer = new MultiDispatchCommandComparer();
         }
 
         [Test]
         public void Same_commands_are_equal()
         {
-            Assert.That(new StuffCommand(), Is.EqualTo(new StuffCommand()).Using(_comparer));
-            Assert.That(new AsdfCommand(), Is.EqualTo(new AsdfCommand()).Using(_comparer));
+            Assert.That(new SmallCommand(), Is.EqualTo(new SmallCommand()).Using(_comparer));
+            Assert.That(new BigCommand(), Is.EqualTo(new BigCommand()).Using(_comparer));
         }
 
         [Test]
         public void Asdf_is_greater_than_stuff()
         {
-            Assert.That(new AsdfCommand(), Is.GreaterThan(new StuffCommand()).Using(_comparer));
+            Assert.That(new BigCommand(), Is.GreaterThan(new SmallCommand()).Using(_comparer));
         }
 
         [Test]
         public void Stuff_is_less_than_asdf()
         {
-            Assert.That(new StuffCommand(), Is.GreaterThan(new AsdfCommand()).Using(_comparer));
+            Assert.That(new SmallCommand(), Is.GreaterThan(new BigCommand()).Using(_comparer));
         }
     }
 }
